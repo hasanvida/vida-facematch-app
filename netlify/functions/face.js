63ffa25b7058f895442d6ff3c6d4202d49d986f9
hasanvida <hasan@vida.id>
2026-02-11 16:09:19 +0700

    Update face.js

--- netlify/functions/face.js
+++ netlify/functions/face.js
@@ -1,1 +1,60 @@
+const { randomUUID } = require("crypto");
+
+exports.handler = async (event) => {
+
+  try {
+
+    const { action, faceImage } = JSON.parse(event.body);
+
+    // Get token
+    const tokenRes = await fetch(
+      `${process.env.URL}/.netlify/functions/getToken`
+    );
+    const tokenData = await tokenRes.json();
+
+    const token = tokenData.access_token;
+
+    // Build request
+    const body = {
+      transactionId: randomUUID(),
+      action: action,
+      payload: { faceImage }
+    };
+
+    if(action === "search"){
+      body.searchOptions = {
+        threshold: 0.81,
+        maxResult: 3
+      };
+    }
+
+    // Call VIDA
+    const vidaRes = await fetch(
+      "https://my-services-sandbox.np.vida.id/api/v1/face/blacklist",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(body)
+      }
+    );
+
+    const text = await vidaRes.text();
+
+    return {
+      statusCode: 200,
+      body: text
+    };
+
+  } catch(err){
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    };
+  }
+};
 
