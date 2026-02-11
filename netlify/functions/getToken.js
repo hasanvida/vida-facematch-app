084b28d843e5e1023d4865b82e70ef1c75fdbde6
hasanvida <hasan@vida.id>
2026-02-11 16:09:06 +0700

    Update getToken.js

--- netlify/functions/getToken.js
+++ netlify/functions/getToken.js
@@ -1,1 +1,27 @@
+exports.handler = async () => {
+
+  const auth = Buffer.from(
+    process.env.VIDA_CLIENT_ID + ":" +
+    process.env.VIDA_CLIENT_SECRET
+  ).toString("base64");
+
+  const response = await fetch(
+    "https://qa-sso.vida.id/auth/realms/vida/protocol/openid-connect/token",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${auth}`,
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: "grant_type=client_credentials&scope=roles"
+    }
+  );
+
+  const text = await response.text();
+
+  return {
+    statusCode: 200,
+    body: text
+  };
+};
 
